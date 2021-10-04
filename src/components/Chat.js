eb4264ab4205c608d7392eb727b6c89e3d451229
Matt O'Toole <mattjotoole@gmail.com>
2021-10-04 07:36:20 +0100

    Chat component.

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,7 +1,45 @@
-import React from "react";
+import { AttachFile, SearchOutlined } from "@mui/icons-material";
+import MoreVert from "@mui/icons-material/MoreVert";
+import { Avatar, IconButton } from "@mui/material";
+import React, { useState, useEffect } from "react";
 
 const Chat = () => {
-  return <div></div>;
+  const [seed, setSeed] = useState("");
+
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() * 999));
+  }, []);
+
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+        <div className="chat__headerInfo">
+          <h3>Room Name</h3>
+          <p>Last seen at...</p>
+        </div>
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+      <div className="chat__body">
+        <p className={`chat__message ${true && "chat__receiver"}`}>
+          <span className="chat__name">Matt</span>
+          Hey!
+          <span className="chat__timestamp">3:52pm</span>
+        </p>
+      </div>
+      <div className="chat__footer"></div>
+    </div>
+  );
 };
 
 export default Chat;
